110b64237e38c087e7089f97ee9edd41ddc2850d
Илья Касаткин <IKKasatkin@mai.ru>
2020-11-16 14:57:57 +0300

    Изменил вывод информации о wifi сетях. Добавил свой ArrayAdapter

---
+++ Wi_Fi_scan/Element.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Wi_Fi_scan
+{
+    class Element
+    {
+
+        string title;
+        string security;
+        int level;
+
+        public Element(string title, string security, int level)
+        {
+            this.title = title;
+            this.security = security;
+            this.level = level;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Security { get => security; set => security = value; }
+        public int Level { get => level; set => level = value; }
+    }
+}
--- Wi_Fi_scan/MainActivity.cs
+++ Wi_Fi_scan/MainActivity.cs
@@ -7,45 +7,103 @@ using Android.Net.Wifi;
 using Android.Content;
 using System.Collections.Generic;
 using System.Linq;
+using static Android.Resource;
+using Android.Views;
+using System;
+using Android.Support.Design.Widget;
 
 namespace Wi_Fi_scan
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    
     public class MainActivity : AppCompatActivity
     {
-        static List<string> scanResultList = new List<string>();
+        static Element[] nets;
+        private WifiManager wifiManager;
+        private IList<ScanResult> wifilist;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            Button btnWiFi = FindViewById<Button>(Resource.Id.btn_wifi);
+            FloatingActionButton fab = (FloatingActionButton)FindViewById(Resource.Id.btn_wifi);
 
-            btnWiFi.Click += (sender, e) =>
+            fab.Click += (sender, e) =>
             {
-
-                var intent = new Intent(this, typeof(WiFiListActivity));
-                WifiManager wifi = (WifiManager)GetSystemService(Context.WifiService);
-                ScanReceiver scanReceiver = new ScanReceiver();
-                RegisterReceiver(scanReceiver, new IntentFilter(WifiManager.ScanResultsAvailableAction));
-                wifi.StartScan();
-                intent.PutStringArrayListExtra("wifi_list", scanResultList);
-                StartActivity(intent);
-            };
-            
+                detectWifi();
+            }; 
         }
-        //[BroadcastReceiver(Enabled = true, Exported = false)]
-        public class ScanReceiver : BroadcastReceiver
+
+        public void detectWifi()
         {
-            public override void OnReceive(Context context, Intent intent)
+            this.wifiManager = (WifiManager)GetSystemService(Context.WifiService);
+            this.wifiManager.StartScan();
+            this.wifilist = this.wifiManager.ScanResults;
+
+            nets = new Element[wifilist.Count];
+            int i = 0;
+            foreach (ScanResult wifinetwork in wifilist)
             {
-                WifiManager wifi = (WifiManager)Android.App.Application.Context.GetSystemService(Context.WifiService);
-                IList<ScanResult> scanwifinetworks = wifi.ScanResults;
-                foreach (ScanResult wifinetwork in scanwifinetworks)
+                string ssid = wifinetwork.Ssid;
+                string security = wifinetwork.Bssid;
+                int level = wifinetwork.Level;
+                nets[i] = new Element(ssid, security, level);
+                i++;
+            }
+            /*for (int i = 0; i < wifilist.Count; i++)
+            {
+                string item = wifilist[i].ToString();
+                string[] vector_item = item.Split(",");
+                string item_essid = vector_item[0];
+                string item_capabilities = vector_item[2];
+                string item_level = vector_item[3];
+                string ssid = item_essid.Split(": ")[1];
+                string security = item_capabilities.Split(": ")[1];
+                string level = item_level.Split(":")[1];
+                nets[i] = new Element(ssid, security, level);
+            }*/
+
+            AdapterElements adapterElements = new AdapterElements(this);
+            ListView netList = (ListView)FindViewById(Resource.Id.listItems);
+            netList.Adapter = adapterElements;
+        }
+
+        public class AdapterElements:ArrayAdapter<Object>
+        {
+            Activity context;
+            public AdapterElements(Activity context) : base(context, Resource.Layout.items, nets)
+            {
+                this.context = context;
+            }
+            public override View GetView(int position, View convertView, ViewGroup parent)
+            {
+                LayoutInflater inflater = context.LayoutInflater;
+                View item = inflater.Inflate(Resource.Layout.items, null);
+
+                TextView tvSsid = (TextView)item.FindViewById(Resource.Id.tvSSID);
+                tvSsid.Text = nets[position].Title;
+
+                TextView tvSecurity = (TextView)item.FindViewById(Resource.Id.tvSecurity);
+                tvSecurity.Text = nets[position].Security;
+
+                TextView tvLevel = (TextView)item.FindViewById(Resource.Id.tvLevel);
+                int level = nets[position].Level;
+
+                int i = level;
+                if (i > -50)
                 {
-                    scanResultList.Add(wifinetwork.Ssid);
+                    tvLevel.Text = "Высокий";
                 }
+                else if (i <= -50 && i > -80)
+                {
+                    tvLevel.Text = "Средний";
+                }
+                else if (i <= -80)
+                {
+                    tvLevel.Text = "Низкий";
+                }
+                return item;
             }
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
